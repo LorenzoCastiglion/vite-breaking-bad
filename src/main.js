4a7c30b3c1f73310d2d5ad73f12f1f8e63446289
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-14 17:25:29 +0100

    impostazione esercizio

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '../node_modules/bootstrap/scss/bootstrap.scss'
+
+import './assets/styles/general.scss'
+
+createApp(App).mount('#app')
